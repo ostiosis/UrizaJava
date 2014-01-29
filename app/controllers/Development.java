5646b6222bed1fdead25e08e814a13739385f141
ostiosis <philip.lipman@gmail.com>
2014-01-29 08:57:13 -0500

    javascript new page creation

--- app/controllers/Development.java
+++ app/controllers/Development.java
@@ -1,8 +1,8 @@
 package controllers;
 
 import static play.data.Form.form;
-
 import models.Page;
+import play.Logger;
 import play.mvc.*;
 import views.html.custom.*;
 import views.html.development.*;
@@ -13,7 +13,7 @@ public class Development extends Controller
 	{
         return ok(development.render("Dev Menu"));
     }
-    
+    /**
     public static Result add()
 	{
 		Page newPage = Page.create(
@@ -24,4 +24,18 @@ public class Development extends Controller
 		
 		return ok(custom.render(newPage));
 	}
+    /**/
+    public static Result add(String name, String title, String description)
+	{
+    	Logger.info("reached");
+    	
+		Page newPage = Page.create(
+			name, 
+			title, 
+			description
+		);
+		
+		return ok(custom.render(newPage));
+	}
+	/**/
 }
